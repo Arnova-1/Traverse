6eccfbaf16f25cedc3eafe6bbfaa3daa1989fa5d
Arnova-1 <arnova124@gmail.com>
2025-07-19 11:35:59 +0700

    feat: implement basic UI rendering logic

---
+++ src/ui.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+use crossterm::event::{self, Event, KeyEventKind};
+use ratatui::{DefaultTerminal, Frame};
+
+use crate::state::AppState;
+
+impl AppState {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+}
