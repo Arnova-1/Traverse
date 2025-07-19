d2292790930105e67e7d7565f2e124aa1c66bd5f
Arnova-1 <arnova124@gmail.com>
2025-07-19 17:19:50 +0700

    feat: Initialize TUI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::io;
+
+use crate::state::AppState;
+
 mod state;
 mod fs;
 mod ui;
 mod render;
 mod handle_key;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = AppState::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
